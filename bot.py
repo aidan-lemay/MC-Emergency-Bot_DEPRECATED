abb53059863319c2cdf24bab896f62a0ea4427d9
Aidan LeMay <aidanpathways12@gmail.com>
2022-08-25 08:31:07 -0400

    Updating for Optional Parameters, Error Checking, and Return Formatting

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ import storage
 import pandas as pd
 from requests_html import HTMLSession
 import requests
+from typing import Optional
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="/", intents=intents)
@@ -142,90 +143,123 @@ async def helpme(ctx):
     await ctx.send("```\nRaspberryPiBot Discord Bot Help!\n\nCreated by Aidan LeMay using Discord.py\nhttps://github.com/The-Doctor-Of-11/RaspberryPiBot\n\n__Command Help:__\n/helpme: Display this help window\n/m911 [X#: Optional Quantity]: Returns X# of Monroe County 911 Events from https://www.monroecounty.gov/incidents911.rss with all 'PARKING INCIDENT's filtered out\n/h911 [X#: Optional Quantity]: Returns X# of Henrietta area 911 Events from https://www.monroecounty.gov/incidents911.rss with all 'PARKING INCIDENT's filtered out\n/r911 [X#: Optional Quantity]: Returns X# of Rochester area 911 Events from https://www.monroecounty.gov/incidents911.rss with all 'PARKING INCIDENT's filtered out\n/a911 [X#: Optional Quantity]: Returns X# of Monroe County 911 Events from https://www.monroecounty.gov/incidents911.rss with no data filtered out\n\nVisit the creator here! https://aidanlemay.com/```")
 
 @bot.command()
-async def m911(ctx, num=1):
+async def m911(ctx, num: Optional[int]):
+
+    if num == None:
+        num = 1
 
     df = get_feed_monroe()
 
-    if num > 20:
-        num = 20
-    elif num < 1:
-        num = 1
+    fSize = len(df)
 
-    message = "```\nMonroe County 911 Events:\n"
+    if fSize == 0:
+        message = "No Monroe County Events Found"
 
-    # for i in df:
-    j = 0
-    while j < num:
-        message += "-> " + df[j] + "\n\n"
-        j+=1
+    else:
+        if num > fSize:
+            num = fSize
+        elif num < 1:
+            num = 1
 
-    message += "```"    
+        message = "```\nMonroe County 911 Events:\n"
+
+        # for i in df:
+        j = 0
+        while j < num:
+            message += "-> " + df[j] + "\n\n"
+            j+=1
+
+        message += "```"
 
     await ctx.send(message)
 
 @bot.command()
-async def r911(ctx, num=1):
+async def r911(ctx, num: Optional[int]):
 
+    if num == None:
+        num = 1
     df = get_feed_roc()
 
-    if num > 20:
-        num = 20
-    elif num < 1:
-        num = 1
+    fSize = len(df)
 
-    message = "```\nRochester Area 911 Events:\n"
+    if fSize == 0:
+        message = "No Rochester Area Events Found"
 
-    # for i in df:
-    j = 0
-    while j < num:
-        message += "-> " + df[j] + "\n\n"
-        j+=1
+    else:
+        if num > fSize:
+            num = fSize
+        elif num < 1:
+            num = 1
 
-    message += "```"    
+        message = "```\nRochester Area 911 Events:\n"
+
+        # for i in df:
+        j = 0
+        while j < num:
+            message += "-> " + df[j] + "\n\n"
+            j+=1
+
+        message += "```"    
 
     await ctx.send(message)
 
 @bot.command()
-async def h911(ctx, num=1):
+async def h911(ctx, num: Optional[int]):
 
+    if num == None:
+        num = 1
     df = get_feed_hen()
 
-    if num > 20:
-        num = 20
-    elif num < 1:
-        num = 1
+    fSize = len(df)
 
-    message = "```\nHenrietta Area 911 Events:\n"
+    if fSize == 0:
+        message = "No Henrietta Area Events Found"
 
-    # for i in df:
-    j = 0
-    while j < num:
-        message += "-> " + df[j] + "\n\n"
-        j+=1
+    else:
+        if num > fSize:
+            num = fSize
+        elif num < 1:
+            num = 1
 
-    message += "```"    
+        message = "```\nHenrietta Area 911 Events:\n"
+
+        # for i in df:
+        j = 0
+        while j < num:
+            message += "-> " + df[j] + "\n\n"
+            j+=1
+
+        message += "```"    
 
     await ctx.send(message)
 
 @bot.command()
-async def a911(ctx, num=1):
+async def a911(ctx, num: Optional[int]):
 
+    if num == None:
+        num = 1
     df = get_unfiltered()
 
-    if num > 20:
-        num = 20
-    elif num < 1:
-        num = 1
+    fSize = len(df)
 
-    message = "```ALL Monroe County 911 Events:\n"
+    if fSize == 0:
+        message = "No Monroe County Events Found"
 
-    # for i in df:
-    j = 0
-    while j < num:
-        message += "-> " + df[j] + "\n\n"
-        j+=1
+    else:
+        if num > fSize:
+            num = fSize
+        elif num < 1:
+            num = 1
 
-    message += "```"    
+        message = "```ALL Monroe County 911 Events:\n"
+
+        # for i in df:
+        j = 0
+        while j < num:
+            message += "-> " + df[j] + "\n\n"
+            j+=1
+
+        message += "```"   
 
     await ctx.send(message)
 
